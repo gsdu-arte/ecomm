7f2409b6e4019e1fcf3e9d66c830c19817d4d593
Gabriella Duarte <gabriella.duarte@pagonxt.com>
2023-02-06 17:54:56 -0300

    feat: add rota get pelo id

--- product/src/routes/categoriesRoutes.js
+++ product/src/routes/categoriesRoutes.js
@@ -4,6 +4,7 @@ import categoriesController from '../controller/categoriesController.js';
 const categoriesRouter = Router();
 
 categoriesRouter
+  .get('/categories/:id', categoriesController.listCategoryById)
   .get('/categories', categoriesController.listAllCategories)
   .post('/categories', categoriesController.createCategory);
 
