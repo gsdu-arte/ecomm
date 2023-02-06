5ad0f3d71731ae760dc6bf92abbccc47259998c5
Gabriella Duarte <gabriella.duarte@pagonxt.com>
2023-02-06 14:27:49 -0300

    feat: add função que cria nova categoria no controller de categorias

--- product/src/controller/categoriesController.js
+++ product/src/controller/categoriesController.js
@@ -1,10 +1,24 @@
-import categories from '../model/Category.js';
+import categoryModel from '../model/Category.js';
+import validate from '../validations/categoriesValidations.js'
 
 const categoriesController = {
-  listAllProducts: async (_req, res) => {
-    const data = await categories.find();
+  listAllCategories: async (_req, res) => {
+    const data = await categoryModel.find();
   
     return res.status(200).json(data);
+  },
+
+  createCategory: async (req, res) => {
+    const isValid = validate.validateBody(req.body);
+    
+    if (isValid.error) {
+      return res.status(500).json({ message: isValid.error.message})
+    }
+
+    const newCategory = await categoryModel.create(req.body);
+    console.log(newCategory);
+    
+    return res.status(201).json(newCategory);
   }
 }
 
