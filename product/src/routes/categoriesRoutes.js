c211478fb807876790f795566dd065e70e1953dc
Gabriella Duarte <gabriella.duarte@pagonxt.com>
2023-02-06 14:33:31 -0300

    feat: add rota post nas rotas de categorias

--- product/src/routes/categoriesRoutes.js
+++ product/src/routes/categoriesRoutes.js
@@ -3,6 +3,8 @@ import categoriesController from '../controller/categoriesController.js';
 
 const categoriesRouter = Router();
 
-categoriesRouter.get('/categories', categoriesController.listAllProducts);
+categoriesRouter
+  .get('/categories', categoriesController.listAllCategories)
+  .post('/categories', categoriesController.createCategory);
 
 export default categoriesRouter;
