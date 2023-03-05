e9032b30f400504cb182bbba4e82697b603a980f
Gabriella Duarte <gabriella.duarte@pagonxt.com>
2023-03-04 21:10:44 -0300

    fix: altera categoriesController

--- product/src/controller/categoriesController.js
+++ product/src/controller/categoriesController.js
@@ -3,28 +3,50 @@ import validate from '../validations/categoriesValidations.js'
 
 const categoriesController = {
   listAllCategories: async (_req, res) => {
-    const categories = await categoryModel.find();
+    try {
+      const categories = await categoryModel.find();
+
+      if (!categories) {
+        throw new Error('Categorias não encontradas');
+      }
   
-    return res.status(200).json(categories);
+      return res.status(200).json(categories);
+    } catch (error) {
+      if (error.message === 'Categorias não encontradas') {
+        return res.status(404).json(error.message);
+      }
+
+      return res.status(500).json(error.message);
+    }
   },
 
   listCategoryById: async (req, res) => {
-    const category = await categoryModel.findById(req.params.id);
+    try {
+      const category = await categoryModel.findById(req.params.id);
 
-    return res.status(200).json(category);
+      if (!category) {
+        throw new Error('Categoria não encontrada');
+      }
+
+      return res.status(200).json(category);
+    } catch (error) {
+      if (error.message === 'Categoria não encontrada') {
+        return res.status(404).json(error.message);
+      }
+
+      return res.status(500).json(error.message);
+    }
   },
 
   createCategory: async (req, res) => {
-    const isValid = validate.validateBody(req.body);
-    
-    console.log(isValid.error);
-    if (isValid.error) {
-      return res.status(500).json({ message: isValid.error.message})
+    try {
+      const isValid = validate.validateBody(req.body);
+      const newCategory = await categoryModel.create(isValid.value);
+      
+      return res.status(201).json(newCategory);
+    } catch (error) {
+      return res.status(500).json(error.message);
     }
-
-    const newCategory = await categoryModel.create(isValid.value);
-    
-    return res.status(201).json(newCategory);
   }
 }
 
